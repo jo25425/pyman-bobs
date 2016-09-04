0026a6d9653f444c08b0fe30a92f885015de9401
Joanne Robert <joanne@brandwatch.com>
2016-09-04 18:58:46 +0100

    Use grid for initial setup, draw grid, smaller pellets

--- Pyman.py
+++ Pyman.py
@@ -2,10 +2,13 @@
 //TODO
 [X] Limit movement to window bounds
 [X] Add speed booster, lower default speed, back to normal after countdown
+[X] Render initial world as grid from array
 [ ] Add obstacles: collision that prevents movement rather than kills object
+[ ] Place obstacles from maps
+[ ] Add new prizes with different values (fruit? mice?)
 [ ] Make snake face other way when going right vs. left
 [ ] Nicer text: reserved area with frame and nicer colour scheme
-[ ] Add second player
+[ ] Add second player / enemies (AI)
 '''
 
 import sys
@@ -24,17 +27,28 @@ if not pygame.mixer:
 # Constants
 BOOSTER_DURATION = 5
 BOOSTER_STRENGTH = 3
+GRID_SPACING = 64
+GRID_COLOR = (255, 255, 0)
+GRID_THICKNESS = 1.5
+MOVE_KEYS = [
+    locals.K_RIGHT,
+    locals.K_LEFT,
+    locals.K_UP,
+    locals.K_DOWN
+]
 
 
 class PyMan:
     '''The main PyMan class: This class handles the initialisation and creating
     of the game'''
 
-    def __init__(self, width=640, height=480):
+    def __init__(self, width=640, height=512):
         pygame.init()
 
         self.width = width
         self.height = height
+        self.grid = [[0 for x in range(int(self.width / GRID_SPACING))]
+                     for y in range(int(self.height / GRID_SPACING))]
 
         # Create the screen
         # set_mode(resolution=(0,0), flags=0, depth=0) -> Surface
@@ -44,6 +58,7 @@ class PyMan:
 
     def go(self):
         '''This is the main game method'''
+        self.load_map()
         self.load_sprites()
 
         # Define keystroke event delay and interval
@@ -58,8 +73,6 @@ class PyMan:
         if pygame.font:
             font = pygame.font.Font(None, 36)
 
-        move_keys = [locals.K_RIGHT, locals.K_LEFT, locals.K_UP, locals.K_DOWN]
-
         # The main game loop contains all in-game actions
         while True:
 
@@ -70,7 +83,7 @@ class PyMan:
 
             # Keyboard input handling
             pressed = pygame.key.get_pressed()
-            pressed_move_keys = [key for key in move_keys if pressed[key]]
+            pressed_move_keys = [key for key in MOVE_KEYS if pressed[key]]
             if pressed_move_keys:
                 self.snake.move(pressed_move_keys)
 
@@ -103,6 +116,9 @@ class PyMan:
 
     def render(self, font):
         self.screen.blit(self.background, (0, 0))
+
+        self.draw_grid()
+
         self.pellet_sprites.draw(self.screen)
         self.booster_sprites.draw(self.screen)
         self.snake_sprites.draw(self.screen)
@@ -115,27 +131,41 @@ class PyMan:
 
         pygame.display.flip()
 
+    def load_map(self):
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[i])):
+                if random.random() < 0.9:
+                    self.grid[i][j] = 'P'
+                else:
+                    self.grid[i][j] = 'B'
+
+    def draw_grid(self):
+        closed = False
+
+        for i in range(1, len(self.grid)):
+            points = [(0, i * GRID_SPACING), (self.width, i * GRID_SPACING)]
+            pygame.draw.lines(self.screen, GRID_COLOR,
+                              closed, points, GRID_THICKNESS)
+        for j in range(1, len(self.grid[i])):
+            points = [(j * GRID_SPACING, 0), (j * GRID_SPACING, self.width)]
+            pygame.draw.lines(self.screen, GRID_COLOR,
+                              closed, points, GRID_THICKNESS)
+
     def load_sprites(self):
         # Snake
         self.snake_sprites = pygame.sprite.RenderPlain((self.snake))
 
-        item_width, item_height = 64, 64
-        n_horizontal = int(self.width / item_width)
-        n_vertical = int(self.height / item_height)
-
-        # Pellets
+        # Pellets, boosters
         self.pellet_sprites = pygame.sprite.Group()
-
-        # Boosters
         self.booster_sprites = pygame.sprite.Group()
 
-        for x in range(n_horizontal):
-            for y in range(n_vertical):
-                r = pygame.Rect(x * item_width, y * item_height,
-                                item_width, item_height)
-                if random.random() < 0.9:
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[i])):
+                r = pygame.Rect(j * GRID_SPACING, i * GRID_SPACING,
+                                GRID_SPACING, GRID_SPACING)
+                if self.grid[i][j] == 'P':
                     self.pellet_sprites.add(Pellet(r))
-                else:
+                elif self.grid[i][j] == 'B':
                     self.booster_sprites.add(Booster(r))
 
 
